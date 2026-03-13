363d6296125305b19161ddb0731b7b343dc438aa
diana-ab <dianabruk@gmail.com>
2026-03-13 21:52:00 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import FeedPage from "./pages/FeedPage";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
