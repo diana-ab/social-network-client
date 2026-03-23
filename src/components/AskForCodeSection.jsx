3b6c07dff20b49beb93148e96115d8a085139ecf
Shimon98 <lagziel.98@gmail.com>
2026-03-23 16:23:34 +0200

    up

--- src/components/AskForCodeSection.jsx
+++ src/components/AskForCodeSection.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function AskForCodeSection({
                                sendCodeFunction,
@@ -10,17 +10,23 @@ function AskForCodeSection({
     const [secondsLeft, setSecondsLeft] = useState(60);
     const [isSending, setIsSending] = useState(false);
 
+    const hasSentInitialCode =useRef(false);
+    const isSendingRef = useRef(false);
+
+
     const askForCode = async () => {
-        if (isSending) {
+        console.log("askForCode  " + isSendingRef.current);
+        if (isSendingRef.current) {
             return;
         }
-
+        isSendingRef.current = true;
         setIsSending(true);
 
         try {
             const result = await sendCodeFunction(requestData);
-            setSecondsLeft(60);
+            console.log(result);
             if (result.success) {
+                setSecondsLeft(60);
                 setSuccessMessage("A new code was sent.");
             } else {
                 setErrorCodeMessage(result.errorCode);
@@ -28,11 +34,16 @@ function AskForCodeSection({
         } catch (error) {
             setApiErrorMessage(error);
         } finally {
+            isSendingRef.current = false;
             setIsSending(false);
         }
     };
 
     useEffect(() => {
+        if (hasSentInitialCode.current) {
+            return;
+        }
+        hasSentInitialCode.current = true;
         askForCode();
     }, []);
 
@@ -60,7 +71,8 @@ function AskForCodeSection({
         <p>
             Didn’t get a code?{" "}
             <button type="button" onClick={handleResendClick} disabled={secondsLeft > 0 || isSending}>
-                {secondsLeft > 0 ? `Send again in ${secondsLeft}s` : "Send again"}
+                {isSending ? "Sending..."
+                    : secondsLeft > 0 ? `Send again in ${secondsLeft}s` : "Send again"}
             </button>
         </p>
     );
